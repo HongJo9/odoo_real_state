f4632d808654c6b6e8dff6d450ed7f107f8d2d24
HongJo9 <luis.chavez.kook@gmail.com>
2025-10-15 16:02:54 -0500

    primero commit del clon

--- real_estate/models/estate_property_inherit.py
+++ real_estate/models/estate_property_inherit.py
@@ -14,4 +14,6 @@ class EstatePropertyInherit(models.Model):
             record.message_post(body="Nueva propiedad registrada")
         return record
     
+    ###########################################################################
+    
     
